470e720ebcfde5a18f332439d0e640c86321b6b2
tomascomenta-blip <tomascomenta@gmail.com>
2026-04-26 16:16:20 -0500

    fix: custom title bar + clean UI

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 // Views/MainWindow.xaml.cs
 // =============================================================================
 using System.Windows;
+using System.Windows.Input;
 using Microsoft.Extensions.DependencyInjection;
 using ReaperPluginManager.ViewModels;
 
@@ -16,5 +17,32 @@ namespace ReaperPluginManager.Views
             DataContext = vm;
             Loaded += async (_, _) => await vm.InitializeAsync();
         }
+
+        // Permite mover la ventana arrastrando la barra de título
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                // Doble clic: maximizar/restaurar
+                WindowState = WindowState == WindowState.Maximized
+                    ? WindowState.Normal
+                    : WindowState.Maximized;
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+            => WindowState = WindowState.Minimized;
+
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+            => WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+            => Close();
     }
 }
